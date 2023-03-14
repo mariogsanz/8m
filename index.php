f511916b9a093ae6a6a5a80e297d04b975231354
FlowRiberaAzul <mariogsanz@hotmail.com>
2023-03-14 20:49:48 +0100

    update

--- index.php
+++ index.php
@@ -1,3 +1,29 @@
+<?php
+    if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+        if (isset($_POST['submit'])){
+            $opcion = $_POST['estudios'];
+
+            if (!empty($opcion)){
+                switch ($opcion) {
+                    case '1':
+                        header('Location: opcion1.php');
+                        break;
+                    case '2':
+                        header('Location: opcion2.php');
+                        break;
+                    case '3':
+                        header('Location: opcion3.php');
+                        break;
+                    
+                    default:
+                        header('Location: index.php');
+                        break;
+                }
+            }
+        }
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -15,26 +41,43 @@
 
 <body>
     <div class="container-fluid h-100 d-flex flex-column align-items-center p-0">
-        <div class="container-fluid p-2 red text-center">
+        <div class="container-fluid red p-2 text-center">
             <h1 class="display-4">Margaret</h1>
         </div>
-        <div class="container-fluid d-flex flex-column align-items-center peach p-2 flex-grow-1">
-            <div class="container mt-4">
-                <a href="titulacion.php" class="col-sm btn purple m-1">
-                    ¿Qué puedo estudiar con mi titulación?
-                </a>
-                <a href="todofp.php" class="col-sm btn purple m-1">
-                    TodoFP
-                </a>
-                <a href="aulamentor.php" class="col-sm btn purple m-1">
-                    AulaMentor
-                </a>
-                <a href="sistema_educativo.php" class="col-sm btn purple m-1">
-                    Sistema Educativo
-                </a>
+        <div class="container-fluid flex-grow-1 peach d-flex flex-column align-items-center">
+            <div id="carousel" class="carousel carousel-dark slide mt-4" data-bs-ride="carousel">
+                <div class="carousel-indicators">
+                    <button type="button" data-bs-target="#carousel" data-bs-slide-to="0" class="active"></button>
+                    <button type="button" data-bs-target="#carousel" data-bs-slide-to="1"></button>
+                </div>
+                <div class="carousel-inner">
+                    <div class="carousel-item active" data-bs-interval="5000">
+                        <h3>Solo el 13% de las programadoras son mujeres.</h3>
+                        <p>En 2025 cerca del 50% de los empleos estarán relacionados con la tecnología. Inés Vázquez y Rosario Ortiz afrontaron el problema creando Adalab, una empresa social diseñada para combatir la brecha de género tecnológica mediante la formación y la integración al mercado laboral. Han conseguido emplear al 94% de sus alumnas.</p>
+                    </div>
+                    <div class="carousel-item" data-bs-interval="5000">
+                        <p>“En apenas cinco años, cerca del 50% de los empleos estarán relacionados con la tecnología. Si no nos metemos, vamos a estar fuera de la empleabilidad en el sector"</p>
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carousel" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon"></span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carousel" data-bs-slide="next">
+                    <span class="carousel-control-next-icon"></span>
+                </button>
+            </div>
+            <div class="mt-4">
+                <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post" class="d-flex flex-column align-items-center">
+                    <select name="estudios" id="estudios" class="form-select">
+                        <option selected>Selecciona el nivel de estudios</option>
+                        <option value="1">Opcion 1</option>
+                        <option value="2">Opcion 2</option>
+                        <option value="3">Opcion 3</option>
+                    </select>
+                    <input name="submit" type="submit" value="Empieza ya!" class="btn purple mt-5">
+                </form>
             </div>
         </div>
-    </div>
     </div>
 </body>
 
