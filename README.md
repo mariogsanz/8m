86691aa2c56042fc88fc371b7c9a76774db1e640
FlowRiberaAzul <mariogsanz@hotmail.com>
2023-03-14 10:20:05 +0100

    update

---
+++ README.md
@@ -0,0 +1,10 @@
+# MARARET
+Programa Margaret Hamilton (en honor a la primera ingeniera de software)
+
+Con el objetivo de trabajar en la igualdad efectiva, llevamos a cabo una herramienta destinada a fomentar la inserción laboral en el marco de la iniciativa, tanto para mujeres amas de casa con baja cualificación o sin titulación, ¿mayores de 45 años, jóvenes, inmigrantes, como para las mujeres víctimas de violencia de genero y/o doméstica. ? impulsados por las situaciones en las que la mujer ama de casa, reduce su carrera profesional por motivo de los cuidados, lo que hace que su vida laboral sea corta, y por lo tanto las bases reguladoras bajas, lo que se traduce en pensiones mucho menores.??
+
+El objetivo del programa Margaret, es promover la mayor participación en el ámbito económico, laboral y social de aquellas mujeres que quieran desarrollarse personal y profesionalmente, eliminando así situaciones que desemboquen en la exclusión. Asimismo, se pretende conseguir aumentar la confianza y autoestima, además de contar con recursos informativos que sirvan de apoyo en el proceso, como por ejemplo información sobre iniciativas locales, ayudas disponibles, paginas de organizaciones, ofertas de trabajo, formación online…
+
+El programa Margaret es una guía personalizada en cuanto a inserción social y laboral, teniendo siempre en cuenta la situación de partida en la que se encuentre, teniendo como finalidad elevar el perfil de las mujeres en áreas científico-técnicas y STEM. ¿?
+
+Esta iniciativa se debe a que, en un futuro próximo, el 50% de los empleos tendrán relación con la tecnología.
---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Margaret</title>
+    <link rel="shortcut icon" href="assets/img/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+    <div class="container-fluid h-100 d-flex flex-column align-items-center p-0">
+        <div class="container-fluid p-2 red text-center">
+            <h1 class="display-4">Margaret</h1>
+        </div>
+        <div class="container-fluid d-flex flex-column align-items-center peach p-2 flex-grow-1">
+            <div class="container mt-4">
+                <a href="titulacion.php" class="col-sm btn purple m-1">
+                    ¿Qué puedo estudiar con mi titulación?
+                </a>
+                <a href="todofp.php" class="col-sm btn purple m-1">
+                    TodoFP
+                </a>
+                <a href="aulamentor.php" class="col-sm btn purple m-1">
+                    AulaMentor
+                </a>
+                <a href="sistema_educativo.php" class="col-sm btn purple m-1">
+                    Sistema Educativo
+                </a>
+            </div>
+        </div>
+    </div>
+    </div>
+</body>
+
+</html>
